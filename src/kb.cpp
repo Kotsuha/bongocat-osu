e1670fc5f20eb811184b5fc10b03c163c896fcd5
Kotsuha <inciesso@gmail.com>
2020-06-19 09:16:21 +0800

    Use simpler code to determine which key to press

--- src/kb.cpp
+++ src/kb.cpp
@@ -1,4 +1,6 @@
 #include "header.hpp"
+#include <fstream>
+#include <vector>
 
 namespace kb
 {
@@ -10,7 +12,10 @@ int left_hand_key_value[NUM], right_hand_key_value[NUM], no_hand_key_value[NUM];
 int left_hand_pos = -1, right_hand_pos = -1;
 bool left_hand_key_is_down[NUM], right_hand_key_is_down[NUM];
 bool left_hand_key_was_down[NUM], right_hand_key_was_down[NUM];
-int curr_left_hand_key_down_order = 0, curr_right_hand_key_down_order = 0, left_hand_key_down_order[NUM] = {0}, right_hand_key_down_order[NUM] = {0};
+std::vector<int> left_hand_key_down_order, right_hand_key_down_order;
+
+std::ofstream log_file;
+bool is_debug = false;
 
 bool init()
 {
@@ -43,7 +48,21 @@ bool init()
         }
     }
 
+    if (is_debug)
+    {
+        log_file.open("debug.log");
+    }
+
     return true;
+}
+
+void print_log(const std::string& text)
+{
+    if (!is_debug)
+    {
+        return;
+    }
+    log_file << text << std::endl;
 }
 
 void draw_7K()
@@ -51,37 +70,28 @@ void draw_7K()
     // Draw bg
     window.draw(bg);
 
-    // Draw keys
+    // Get key down info & Draw keys
     for (int i = 0; i < NUM; i++)
     {
         left_hand_key_is_down[i] = (left_hand_key_value[i] > 0 && GetKeyState(left_hand_key_value[i]) & WM_KEYDOWN);
-        right_hand_key_is_down[i]= (right_hand_key_value[i] > 0 && GetKeyState(right_hand_key_value[i]) & WM_KEYDOWN);
+        right_hand_key_is_down[i] = (right_hand_key_value[i] > 0 && GetKeyState(right_hand_key_value[i]) & WM_KEYDOWN);
 
         if (!left_hand_key_was_down[i] && left_hand_key_is_down[i])
         {
-            curr_left_hand_key_down_order++;
-            if (curr_left_hand_key_down_order == INT_MIN)
-            {
-                for (int j = 0; j < NUM; j++)
-                {
-                    left_hand_key_down_order[j] += NUM;
-                }
-                curr_left_hand_key_down_order += NUM;
-            }
-            left_hand_key_down_order[i] = curr_left_hand_key_down_order;
+            left_hand_key_down_order.insert(left_hand_key_down_order.begin(), i);
         }
+        else if (left_hand_key_was_down[i] && !left_hand_key_is_down[i])
+        {
+            left_hand_key_down_order.erase(std::find(left_hand_key_down_order.begin(), left_hand_key_down_order.end(), i));
+        }
+
         if (!right_hand_key_was_down[i] && right_hand_key_is_down[i])
         {
-            curr_right_hand_key_down_order++;
-            if (curr_right_hand_key_down_order == INT_MIN)
-            {
-                for (int j = 0; j < NUM; j++)
-                {
-                    right_hand_key_down_order[j] += NUM;
-                }
-                curr_right_hand_key_down_order += NUM;
-            }
-            right_hand_key_down_order[i] = curr_right_hand_key_down_order;
+            right_hand_key_down_order.insert(right_hand_key_down_order.begin(), i);
+        }
+        else if (right_hand_key_was_down[i] && !right_hand_key_is_down[i])
+        {
+            right_hand_key_down_order.erase(std::find(right_hand_key_down_order.begin(), right_hand_key_down_order.end(), i));
         }
 
         if (left_hand_key_is_down[i])
@@ -99,28 +109,11 @@ void draw_7K()
     }
 
     // Determine hand positions
-    int tmp_most_recent_order = INT_MIN;
-    left_hand_pos = -1;
-    for (int i = 0; i < NUM; i++)
-    {
-        if (left_hand_key_is_down[i] && left_hand_key_down_order[i] >= tmp_most_recent_order)
-        {
-            tmp_most_recent_order = left_hand_key_down_order[i];
-            left_hand_pos = i;
-        }
-    }
-    right_hand_pos = -1;
-    for (int i = 0; i < NUM; i++)
-    {
-        if (right_hand_key_is_down[i] && right_hand_key_down_order[i] >= tmp_most_recent_order)
-        {
-            tmp_most_recent_order = right_hand_key_down_order[i];
-            right_hand_pos = i;
-        }
-    }
+    left_hand_pos = left_hand_key_down_order.empty() ? -1 : *left_hand_key_down_order.begin();
+    right_hand_pos = right_hand_key_down_order.empty() ? -1 : *right_hand_key_down_order.begin();
 
     // Draw hands
-    window.draw( left_hand_pos == -1 ?  left_hand_up :  left_hand[ left_hand_pos]);
+    window.draw(left_hand_pos == -1 ? left_hand_up : left_hand[left_hand_pos]);
     window.draw(right_hand_pos == -1 ? right_hand_up : right_hand[right_hand_pos]);
 
     // Update key down
